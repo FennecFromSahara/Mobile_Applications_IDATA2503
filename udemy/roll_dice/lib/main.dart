9dc34629cf6440a71c1b039600eabb93e291de83
FennecFromSahara <143624395+FennecFromSahara@users.noreply.github.com>
2023-09-04 21:09:14 +0200

    Video 50 completed

--- udemy/roll_dice/lib/main.dart
+++ udemy/roll_dice/lib/main.dart
@@ -1,29 +1,16 @@
 import 'package:flutter/material.dart';
 
+import 'package:roll_dice/gradient_container.dart';
+
 void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 183, 125, 58),
-                Color.fromARGB(255, 220, 131, 72),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              'Hello World!',
-              style: TextStyle(
-                fontSize: 28,
-                color: Colors.white,
-              ),
-            ),
-          ),
+        body: GradientContainer(
+          colors: const [
+            Color.fromARGB(255, 183, 125, 58),
+            Color.fromARGB(255, 220, 131, 72),
+          ],
         ),
       ),
     ),
